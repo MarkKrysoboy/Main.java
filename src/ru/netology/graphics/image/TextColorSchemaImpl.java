7c97446f94356e6ee404025352e35dd3833198be
MarkKrysoboy <93878800+MarkKrysoboy@users.noreply.github.com>
2021-11-20 10:14:22 +0300

    Update TextColorSchemaImpl.java

--- src/ru/netology/graphics/image/TextColorSchemaImpl.java
+++ src/ru/netology/graphics/image/TextColorSchemaImpl.java
@@ -5,7 +5,7 @@ public class TextColorSchemaImpl implements TextColorSchema {
     @Override
     public char convert(int color) {
         return color < 32 ? '#' : (color < 63 ? '$' : (color < 95 ? '@' : (color < 127 ? '%' : (color < 159 ? '*' :
-                (color < 191 ? '+' : (color < 123 ? '-' : '\''))))));
+                (color < 191 ? '+' : (color < 256 ? '-' : '\''))))));
     }
 }
 
